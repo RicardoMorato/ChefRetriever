0fb95c8ae4affd07fd1739351f0665c32c3bc65e
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-10-21 21:02:02 -0300

    main(index.ts): Changed Port.

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(3333, () => {
+  console.log('Listening on port 3333');
 });
