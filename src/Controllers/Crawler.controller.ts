733324da56d3b27f4675fc398244ea31d1071b2c
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-10-22 12:12:41 -0300

    main(Crawler.controller.ts): Retrieving ALL data from meet a chef.

--- src/Controllers/Crawler.controller.ts
+++ src/Controllers/Crawler.controller.ts
@@ -36,7 +36,7 @@ export default class Crawler {
 
     await page.goto(url);
 
-    const aux = await page.evaluate(() => {
+    const chefs = await page.evaluate(() => {
       const chefs = [];
 
       const motherDiv = document.querySelector(
@@ -49,93 +49,38 @@ export default class Crawler {
 
       const arrayLength = arrayOfCards?.length;
 
-      for (let index = 0; index < arrayLength; index++) {
-        const chefName = arrayOfCards[index]?.querySelector(
-            '.Styled__ListingHeadingH2-sc-1nslgi0-6.khsIQU',
-        )?.innerHTML;
+      if (arrayOfCards && arrayLength) {
+        for (let index = 0; index < arrayLength; index++) {
+          const chefName = arrayOfCards[index]?.querySelector(
+              '.Styled__ListingHeadingH2-sc-1nslgi0-6.khsIQU',
+          )?.innerHTML;
 
-        const currentChef = {name: chefName};
-        chefs.push(currentChef);
+          const chefSpecialty = arrayOfCards[index]?.querySelector(
+              '.Styled__ListingSpan-sc-1nslgi0-8.jBqSiQ',
+          )?.innerHTML;
+
+          const chefBioCard = arrayOfCards[index]?.querySelector(
+              '.Styled__ListingDescriptionP-sc-1nslgi0-13.cAtssD',
+          );
+          const chefBio = (chefBioCard?.firstChild as Element).innerHTML;
+
+          const chefLocation = arrayOfCards[index]?.querySelector(
+              '.Styled__LocationNameSpan-sc-1nslgi0-10.cAUUyt',
+          )?.innerHTML;
+
+          const currentChef = {
+            Name: chefName,
+            Specialty: chefSpecialty,
+            Bio: chefBio,
+            Location: chefLocation,
+          };
+          chefs.push(currentChef);
+        }
       }
 
       return chefs;
     });
 
-    const chefName = await page.evaluate(() => {
-      const motherDiv = document.querySelector(
-          '.HomePageComponents__BaseDiv-sc-13amrf6-6.jDqYnR',
-      );
-
-      const card = motherDiv?.querySelector(
-          '.Styled__ListingInfoContainer-sc-1nslgi0-5.iktAgy',
-      );
-
-      const name = card?.querySelector(
-          '.Styled__ListingHeadingH2-sc-1nslgi0-6.khsIQU',
-      )?.innerHTML;
-
-      return name;
-    });
-
-    const chefSpecialty = await page.evaluate(() => {
-      const motherDiv = document.querySelector(
-          '.HomePageComponents__BaseDiv-sc-13amrf6-6.jDqYnR',
-      );
-
-      const card = motherDiv?.querySelector(
-          '.Styled__ListingInfoContainer-sc-1nslgi0-5.iktAgy',
-      );
-
-      const specialty = card?.querySelector(
-          '.Styled__ListingSpan-sc-1nslgi0-8.jBqSiQ',
-      )?.innerHTML;
-
-      return specialty;
-    });
-
-    const chefBio = await page.evaluate(() => {
-      const motherDiv = document.querySelector(
-          '.HomePageComponents__BaseDiv-sc-13amrf6-6.jDqYnR',
-      );
-
-      const card = motherDiv?.querySelector(
-          '.Styled__ListingInfoContainer-sc-1nslgi0-5.iktAgy',
-      );
-
-      const aux = card?.querySelector(
-          '.Styled__ListingDescriptionP-sc-1nslgi0-13.cAtssD',
-      );
-
-      const bio = aux?.firstChild;
-
-      return (bio as Element).innerHTML;
-    });
-
-    const chefLocation = await page.evaluate(() => {
-      const motherDiv = document.querySelector(
-          '.HomePageComponents__BaseDiv-sc-13amrf6-6.jDqYnR',
-      );
-
-      const card = motherDiv?.querySelector(
-          '.Styled__ListingInfoContainer-sc-1nslgi0-5.iktAgy',
-      );
-
-      const location = card?.querySelector(
-          '.Styled__LocationNameSpan-sc-1nslgi0-10.cAUUyt',
-      )?.innerHTML;
-
-      return location;
-    });
-
-    console.log(aux);
-
-    const chef = this.chefConstructor(
-        chefName,
-        chefLocation,
-        chefSpecialty,
-        chefBio,
-    );
-
-    res.json(chef);
+    res.json(chefs);
   };
 }
